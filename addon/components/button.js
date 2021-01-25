373488c72b0980cc262213c8e82831031f002fde
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2021-01-26 00:36:29 +0530

    1. Adds variants.

--- addon/components/button.js
+++ addon/components/button.js
@@ -1,4 +1,11 @@
 import Component from '@glimmer/component';
 
 export default class ButtonComponent extends Component {
+    get isElevated() {
+        return (this.args?.elevated ?? true) && this.variant != "text";
+    }
+
+    get variant() {
+        return this.args?.variant ?? "contained";
+    }
 }
